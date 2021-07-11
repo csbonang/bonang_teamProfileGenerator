78f0a17dfc1adad2cefa479563c840af38ab3552
csbonang <csgbonang@gmail.com>
2021-07-11 13:36:28 -0700

    included folders to organize docs and completed layout of classes

---
+++ Node.js
@@ -0,0 +1,47 @@
+// TODO: command line application that takes information 
+/* Command line application 
+    1. Enter the team manager's 
+        Ask for Name 
+        Position 
+        Id
+        Email 
+        GitHub 
+    2. Menu appears and exit option
+        Engineer 
+        Intern 
+        Exit 
+
+    3. Fill position 
+   if Position == Engineer 
+        Ask for Name 
+        Position 
+        Id
+        Email 
+        GitHub 
+    else if Position == Intern
+         Ask for Name 
+        Position 
+        Id
+        Email 
+        School 
+    4. Menu appears and exit option
+         Engineer 
+         Intern 
+         Exit 
+
+*/ 
+
+// HTML generated 
+    // Click on email 
+        // opens email with it populated with to (refer to updated profile)
+    // Click on GitHub 
+        // GitHub profile opens 
+
+
+
+// write a unit test 
+
+
+// Approach
+
+// TODO: create classes 
