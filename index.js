57af144c9964a5d88d654d26cebbdbf329cd088e
csbonang <csgbonang@gmail.com>
2021-07-12 07:36:33 -0700

    added exports code to classes, completed questions for engineers and interns, prompt is completed

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); 
+const Employee = require("./lib/Employee"); 
+const Manager = require("./lib/Manager"); 
+const Engineer = require("./lib/Engineer"); 
+const Intern = require("./lib/Intern"); 
+const chalk = require("chalk"); 
+
 
 // prompt the user: array of objects 
 console.log("Time to build team"); 
@@ -7,12 +13,12 @@ console.log("Time to build team");
 const questions = [
     {
         type: "input", 
-        message: "What is the team manager's name? ", 
+        message: chalk.rgb(249, 38, 249)("What is the team manager's name? "), 
         name: 'tMName'
     },
     {
         type: "input", 
-        message: "What is the team manager's id? ", 
+        message: chalk.blue("What is the team manager's id? "), 
         name: "tMId"
     },
     {
@@ -30,42 +36,136 @@ const questions = [
         message: "Which type of team member would you like to add? ", 
         name: "team_member", 
         choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
-    },
+    }
 ]
+
+// questions for engineer 
+// Ask for 
+// Name 
+// Id
+// Email 
+// GitHub 
+const eng_questions = [
+    {
+        type: "input",
+        message: "Please Enter engineer's name: ",
+        name: "engName"
+    },
+    {
+        type: "input",
+        message: "Please Enter engineer's id number: ",
+        name: "engId"
+    },
+    {
+        type: "input",
+        message: "Please Enter engineer's email ",
+        name: "engEmail"
+    },
+    {
+        type: "input",
+        message: "Please Enter engineer's GitHub username: ",
+        name: "engGitHub"
+    },
+    {
+        type: "list", 
+        message: "Which type of team member would you like to add? ", 
+        name: "team_member", 
+        choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+    }
+]
+
+
+const intern_questions = [
+    {
+        type: "input",
+        message: "Please Enter intern's name: ",
+        name: "internName"
+    },
+    {
+        type: "input",
+        message: "Please Enter intern's id number: ",
+        name: "internId"
+    },
+    {
+        type: "input",
+        message: "Please Enter intern's email ",
+        name: "internEmail"
+    },
+    {
+        type: "input",
+        message: "Please Enter the intern's school ",
+        name: "internSchool"
+    },
+    {
+        type: "list", 
+        message: "Which type of team member would you like to add? ", 
+        name: "team_member", 
+        choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+    }
+]
+
 
 function init()
 {
-    inquirer.prompt(questions); 
+    // prompt the users
+    inquirer.prompt(questions)
+    // save the inputs 
+    .then( (res) => {
+        const inputs = res.team_member; 
+        console.log(inputs); 
+        if (res.team_member == "Engineer") 
+        {
+            questionsForEngineers(); 
+            
+        }
+        else if(res.team_member == "Intern")
+        {
+            questionsForIntern(); 
+        }
+      
+
+
+    })
+    
+}
+
+function questionsForEngineers()
+{
+     // prompt the user 
+     inquirer.prompt(eng_questions)
+     .then((resEngineer) => {
+         const engineer = new Engineer(resEngineer.engName, resEngineer.engId, resEngineer.engEmail, resEngineer.engGitHub); 
+         // diagnostics 
+         engineer.printProperties(); 
+         if (resEngineer.team_member == "Engineer") 
+         {
+             questionsForEngineers(); 
+             
+         }
+         else if(resEngineer.team_member == "Intern")
+         {
+             questionsForInterns(); 
+         }
+     })
+
+}
+function questionsForInterns()
+{
+     // prompt the user 
+     inquirer.prompt(intern_questions)
+     .then((resIntern) => {
+         const intern = new Intern(resIntern.internName, resIntern.internId, resIntern.internEmail, resIntern.internSchool); 
+         // diagnostics 
+         engineer.printProperties(); 
+         if (resIntern.team_member == "Engineer") 
+         {
+             questionsForEngineers(); 
+             
+         }
+         else if(resIntern.team_member == "Intern")
+         {
+             questionsForInterns(); 
+         }
+     })
 }
 init(); 
-/* Command line application 
-    1. Enter the team manager's 
-        Ask for Name 
-        Position 
-        Id
-        Email 
-        GitHub 
-    2. Menu appears and exit option
-        Engineer 
-        Intern 
-        Exit 
-
-    3. Fill position 
-   if Position == Engineer 
-        Ask for Name 
-        Position 
-        Id
-        Email 
-        GitHub 
-    else if Position == Intern
-         Ask for Name 
-        Position 
-        Id
-        Email 
-        School 
-    4. Menu appears and exit option
-         Engineer 
-         Intern 
-         Exit 
-
-*/ 
